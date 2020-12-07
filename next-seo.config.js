22dcaa433a64848a3bf385420d4f226f6377e13e
Javier Szyfer <jszyfer@gmail.com>
2020-12-07 09:40:11 -0300

    og image

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,11 +1,19 @@
 export default {
   title: "door.link",
   description: "Weekly mixtapes from Buenos Aires to the world",
+  canonical: "https://www.door.link",
   openGraph: {
     type: "website",
     locale: "en_IE",
     url: "https://www.door.link",
     site_name: "door.link",
-    image: "https://kapwi.ng/c/9W3vQjNk",
+    images: [
+      {
+        width: "1200",
+        height: "630",
+        url: "https://kapwi.ng/c/9W3vQjNk",
+        alt: "Open graph logo image",
+      },
+    ],
   },
 };
