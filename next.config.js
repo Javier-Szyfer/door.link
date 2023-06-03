c59572914fa7f8eef5da475e24473879f1131328
javvvs <jszynet@gmail.com>
2023-06-03 15:59:00 -0300

    envs and config update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-require("dotenv").config();
-
 module.exports = {
   env: {
     API_URL: process.env.API_URL,
   },
   images: {
-    domains: ["radio-strapi-bucket.s3.sa-east-1.amazonaws.com"],
+    domains: [process.env.BUCKET_URL],
   },
 };
